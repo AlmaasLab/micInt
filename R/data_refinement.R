92f6a1483006be5549e229ffdbe3f601e2b552fa
Jakob Peder Pettersen <jakobpeder.pettersen@gmail.com>
2019-06-03 18:36:25 +0200

    Added phyloseq support for refine_data.

--- R/data_refinement.R
+++ R/data_refinement.R
@@ -4,21 +4,23 @@
 #' @param OTU_table The raw OTU table
 #' @param metadataCols The names (character vector) or position (integer) of the
 #' metadata columns to remove from the table
-remove_metadata <- function(OTU_table, metadataCols = c("OTU Id", "taxonomy")) {
-  if (is.character(metadataCols)) {
-    metadata <- which(colnames(OTU_table) %in% metadataCols)
+remove_metadata <-
+  function(OTU_table,
+           metadataCols = c("OTU Id", "taxonomy")) {
+    if (is.character(metadataCols)) {
+      metadata <- which(colnames(OTU_table) %in% metadataCols)
+    }
+    else {
+      metadata <- metadataCols
+    }
+    # Removes the metadata for the data set if there is any
+    if (length(metadata) != 0) {
+      OTU_table <- OTU_table[,-metadata]
+    }
+    refined_table <- as.data.frame(t(OTU_table))
+    colnames(refined_table) <- row.names(OTU_table)
+    return(refined_table)
   }
-  else {
-    metadata <- metadataCols
-  }
-  # Removes the metadata for the data set if there is any
-  if (length(metadata) != 0) {
-    OTU_table <- OTU_table[, -metadata]
-  }
-  refined_table <- as.data.frame(t(OTU_table))
-  colnames(refined_table) <- row.names(OTU_table)
-  return(refined_table)
-}
 
 #' @title cut_abundances
 #'
@@ -40,32 +42,37 @@ remove_metadata <- function(OTU_table, metadataCols = c("OTU Id", "taxonomy")) {
 #'
 #'
 #' @import matrixStats
-cut_abundances <- function(refined_table, abundance_cutoff = 0, cutoff_type = "mean", renormalize = TRUE) {
-  if (is.null(abundance_cutoff)) {
-    # If we do not set an abundance cutoff, we skip the process entierly
+cut_abundances <-
+  function(refined_table,
+           abundance_cutoff = 0,
+           cutoff_type = "mean",
+           renormalize = TRUE) {
+    if (is.null(abundance_cutoff)) {
+      # If we do not set an abundance cutoff, we skip the process entierly
+      return(refined_table)
+    }
+    m_refined_table <- as.matrix(refined_table)
+    abundances <- switch(
+      cutoff_type,
+      mean = colMeans(refined_table),
+      median = colMedians(m_refined_table),
+      max = colMaxs(m_refined_table),
+      numberNonZero = colSums(m_refined_table != 0),
+      proprotionNonZero = colMeans(m_refined_table != 0)
+    )
+    refined_table <- refined_table[, abundances > abundance_cutoff]
+    if (renormalize) {
+      # Renormalizes the table in order for the rows to sum to 1
+      refined_table <- renormalize(refined_table)
+    }
     return(refined_table)
   }
-  m_refined_table <- as.matrix(refined_table)
-  abundances <- switch(cutoff_type,
-    mean = colMeans(refined_table),
-    median = colMedians(m_refined_table),
-    max = colMaxs(m_refined_table),
-    numberNonZero = colSums(m_refined_table != 0),
-    proprotionNonZero = colMeans(m_refined_table != 0)
-  )
-  refined_table <- refined_table[, abundances > abundance_cutoff]
-  if (renormalize) {
-    # Renormalizes the table in order for the rows to sum to 1
-    refined_table <- renormalize(refined_table)
-  }
-  return(refined_table)
-}
 #' @title Refine raw OTU table
 #' @description
 #' Removes metadata from OTU table and cuts off the least abundant
 #' species, defined by the cutoff parameter
 #'
-#' @param OTU_table The raw OTU table, either as a \code{data.frame} or a \code{matrix} (not phyloseq)
+#' @param OTU_table The raw OTU table, either as a \code{data.frame}, a \code{matrix} or a \code{phyloseq} object
 #' @inheritParams cut_abundances
 #' @param metadataCols The names (character vector) or position (integer) of the
 #' metadata columns to remove from the table
@@ -92,17 +99,36 @@ cut_abundances <- function(refined_table, abundance_cutoff = 0, cutoff_type = "m
 #' \item The row names of the table are the OTU names and the column names are the
 #' sample names
 #' }
+#' Of course, this does not apply to the case when a \code{phyloseq} object is provided as everything
+#'  will be handled automatically
 #'
 #' @export
-refine_data <- function(OTU_table, abundance_cutoff = 0, cutoff_type = "mean", renormalize = TRUE, metadataCols = c("OTU Id", "taxonomy")) {
-  refined_table <- remove_metadata(OTU_table, metadataCols = metadataCols)
-  # Cuts away the least abundant species
-  cut_abundances(refined_table, abundance_cutoff, cutoff_type = cutoff_type, renormalize = renormalize)
-}
+refine_data <-
+  function(OTU_table,
+           abundance_cutoff = 0,
+           cutoff_type = "mean",
+           renormalize = TRUE,
+           metadataCols = c("OTU Id", "taxonomy")) {
+    if (inherits(OTU_table, 'phyloseq')) {
+      refined_table <- phyloseq::otu_table(OTU_table) %>% data.frame()
+      if (!phyloseq::taxa_are_rows(OTU_table)) {
+        refined_table %<>% t()
+      }
+    }
+    else{
+      refined_table <-
+        remove_metadata(OTU_table, metadataCols = metadataCols)
+    }
+    # Cuts away the least abundant species
+    cut_abundances(refined_table,
+                   abundance_cutoff,
+                   cutoff_type = cutoff_type,
+                   renormalize = renormalize)
+  }
 renormalize <- function(table) {
   matrix <- as.matrix(table)
   res <- diag(1 / rowSums(matrix)) %*% matrix
-  table[, ] <- res
+  table[,] <- res
   return(table)
 }
 
@@ -141,26 +167,37 @@ renormalize <- function(table) {
 #' @importFrom utils modifyList write.csv write.csv2
 #'
 #' @export
-output_ccrepe_data <- function(data, taxonomy = NULL, threshold.type = "q", threshold.value = 0.05, output.file = FALSE, filename = NULL,
-                               return.value = TRUE, csv_option = "2", removeDuplicates = TRUE, score_attributes = NULL) {
-  significant_interactions <- create_interaction_table(
-    data = data, taxonomy = taxonomy, threshold.type = threshold.type,
-    threshold.value = threshold.value,
-    removeDuplicates = removeDuplicates, score_attributes = score_attributes
-  )
-  if (output.file) {
-    write.interaction_table(significant_interactions,
-      filename = filename,
-      csv_option = csv_option
+output_ccrepe_data <-
+  function(data,
+           taxonomy = NULL,
+           threshold.type = "q",
+           threshold.value = 0.05,
+           output.file = FALSE,
+           filename = NULL,
+           return.value = TRUE,
+           csv_option = "2",
+           removeDuplicates = TRUE,
+           score_attributes = NULL) {
+    significant_interactions <- create_interaction_table(
+      data = data,
+      taxonomy = taxonomy,
+      threshold.type = threshold.type,
+      threshold.value = threshold.value,
+      removeDuplicates = removeDuplicates,
+      score_attributes = score_attributes
     )
+    if (output.file) {
+      write.interaction_table(significant_interactions,
+                              filename = filename,
+                              csv_option = csv_option)
+    }
+    if (return.value) {
+      return(significant_interactions)
+    }
+    else {
+      return(NULL)
+    }
   }
-  if (return.value) {
-    return(significant_interactions)
-  }
-  else {
-    return(NULL)
-  }
-}
 #' @title Create table of significant interactions
 #'
 #' @description Performs the actual process of making the table.
@@ -182,86 +219,108 @@ output_ccrepe_data <- function(data, taxonomy = NULL, threshold.type = "q", thre
 #' @export
 #'
 #' @inheritParams output_ccrepe_data
-create_interaction_table <- function(data, taxonomy = NULL, threshold.type = "q", threshold.value = 0.05,
-                                     removeDuplicates = TRUE, score_attributes = NULL) {
-  p.values <- as.data.frame(data$p.values)
-  z.stat <- as.data.frame(data$z.stat)
-  sim.score <- as.data.frame(data$sim.score)
-  q.values <- as.data.frame(data$q.values)
-  if (threshold.type == "q") {
-    threshold_matrix <- q.values
-  }
-  else if (threshold.type == "p") {
-    threshold_matrix <- p.values
-  }
-  else {
-    stop("no valid theshold method given, must be 'p' (local p-value)
-         or 'q' (familywise false discovery rate)")
-  }
-  significant_pairs <- which(threshold_matrix < threshold.value, arr.ind = TRUE)
-  significant_interactions <- as.data.frame(matrix(colnames(threshold_matrix)[significant_pairs], ncol = 2))
-  colnames(significant_interactions) <- c("OTU_1", "OTU_2")
-  if (nrow(significant_interactions) == 0) {
-    significant_interactions$sim.score <- numeric()
-    significant_interactions$p.value <- numeric()
-    significant_interactions$q.value <- numeric()
-    significant_interactions$z.stat <- numeric()
-    if (!is.null(taxonomy)) {
-      # Add taxonomy if available
-      significant_interactions$taxonomy_1 <- character()
-      significant_interactions$taxonomy_2 <- character()
-    }
-  }
-  else {
-    # Removing duplicates
-    if (removeDuplicates) {
-      significant_interactions <- significant_interactions[significant_interactions$OTU_1 > significant_interactions$OTU_2, ]
-    }
-    if (!is.null(sim.score) && nrow(sim.score) != 0) {
-      significant_interactions$sim.score <- apply(significant_interactions, 1, function(x) {
-        sim.score[x[1], x[2]]
-      })
-    }
-    if (!is.null(p.values) && nrow(p.values) != 0) {
-      significant_interactions$p.value <- apply(significant_interactions, 1, function(x) {
-        p.values[x[1], x[2]]
-      })
-    }
-    if (!is.null(q.values) && nrow(q.values) != 0) {
-      significant_interactions$q.value <- apply(significant_interactions, 1, function(x) {
-        q.values[x[1], x[2]]
-      })
-    }
-    if (!is.null(z.stat) && nrow(z.stat) != 0) {
-      significant_interactions$z.stat <- apply(significant_interactions, 1, function(x) {
-        z.stat[x[1], x[2]]
-      })
-    }
-    if (!is.null(taxonomy)) {
-      # Add taxonomy if available
-      significant_interactions$taxonomy_1 <- taxonomy[significant_interactions$OTU_1]
-      significant_interactions$taxonomy_2 <- taxonomy[significant_interactions$OTU_2]
-    }
-    # Sorts by dersired significance column
+create_interaction_table <-
+  function(data,
+           taxonomy = NULL,
+           threshold.type = "q",
+           threshold.value = 0.05,
+           removeDuplicates = TRUE,
+           score_attributes = NULL) {
+    p.values <- as.data.frame(data$p.values)
+    z.stat <- as.data.frame(data$z.stat)
+    sim.score <- as.data.frame(data$sim.score)
+    q.values <- as.data.frame(data$q.values)
     if (threshold.type == "q") {
-      significant_interactions <- significant_interactions[order(significant_interactions$q.value), ]
-    } else {
-      significant_interactions <- significant_interactions[order(significant_interactions$p.value), ]
+      threshold_matrix <- q.values
     }
-  }
-  class(significant_interactions) <- c("interaction_table", class(significant_interactions))
-  if (is.null(score_attributes)) {
-    attr(significant_interactions, "measure_name") <- NA
-    attr(significant_interactions, "signed") <- NA
-    attr(significant_interactions, "measure_type") <- NA
-  }
-  else {
-    attr(significant_interactions, "measure_name") <- score_attributes@string
-    attr(significant_interactions, "signed") <- signed <- score_attributes@signed
-    attr(significant_interactions, "measure_type") <- score_attributes@type_measure
-  }
-  return(significant_interactions)
-}
+    else if (threshold.type == "p") {
+      threshold_matrix <- p.values
+    }
+    else {
+      stop(
+        "no valid theshold method given, must be 'p' (local p-value)
+        or 'q' (familywise false discovery rate)"
+      )
+    }
+    significant_pairs <-
+      which(threshold_matrix < threshold.value, arr.ind = TRUE)
+    significant_interactions <-
+      as.data.frame(matrix(colnames(threshold_matrix)[significant_pairs], ncol = 2))
+    colnames(significant_interactions) <- c("OTU_1", "OTU_2")
+    if (nrow(significant_interactions) == 0) {
+      significant_interactions$sim.score <- numeric()
+      significant_interactions$p.value <- numeric()
+      significant_interactions$q.value <- numeric()
+      significant_interactions$z.stat <- numeric()
+      if (!is.null(taxonomy)) {
+        # Add taxonomy if available
+        significant_interactions$taxonomy_1 <- character()
+        significant_interactions$taxonomy_2 <- character()
+      }
+    }
+    else {
+      # Removing duplicates
+      if (removeDuplicates) {
+        significant_interactions <-
+          significant_interactions[significant_interactions$OTU_1 > significant_interactions$OTU_2,]
+      }
+      if (!is.null(sim.score) && nrow(sim.score) != 0) {
+        significant_interactions$sim.score <-
+          apply(significant_interactions, 1, function(x) {
+            sim.score[x[1], x[2]]
+          })
+      }
+      if (!is.null(p.values) && nrow(p.values) != 0) {
+        significant_interactions$p.value <-
+          apply(significant_interactions, 1, function(x) {
+            p.values[x[1], x[2]]
+          })
+      }
+      if (!is.null(q.values) && nrow(q.values) != 0) {
+        significant_interactions$q.value <-
+          apply(significant_interactions, 1, function(x) {
+            q.values[x[1], x[2]]
+          })
+      }
+      if (!is.null(z.stat) && nrow(z.stat) != 0) {
+        significant_interactions$z.stat <-
+          apply(significant_interactions, 1, function(x) {
+            z.stat[x[1], x[2]]
+          })
+      }
+      if (!is.null(taxonomy)) {
+        # Add taxonomy if available
+        significant_interactions$taxonomy_1 <-
+          taxonomy[significant_interactions$OTU_1]
+        significant_interactions$taxonomy_2 <-
+          taxonomy[significant_interactions$OTU_2]
+      }
+      # Sorts by dersired significance column
+      if (threshold.type == "q") {
+        significant_interactions <-
+          significant_interactions[order(significant_interactions$q.value),]
+      } else {
+        significant_interactions <-
+          significant_interactions[order(significant_interactions$p.value),]
+      }
+    }
+    class(significant_interactions) <-
+      c("interaction_table", class(significant_interactions))
+    if (is.null(score_attributes)) {
+      attr(significant_interactions, "measure_name") <- NA
+      attr(significant_interactions, "signed") <- NA
+      attr(significant_interactions, "measure_type") <- NA
+    }
+    else {
+      attr(significant_interactions, "measure_name") <-
+        score_attributes@string
+      attr(significant_interactions, "signed") <-
+        signed <- score_attributes@signed
+      attr(significant_interactions, "measure_type") <-
+        score_attributes@type_measure
+    }
+    return(significant_interactions)
+    }
 
 #'
 #' @title write.interaction_table
@@ -274,15 +333,21 @@ create_interaction_table <- function(data, taxonomy = NULL, threshold.type = "q"
 #' @inheritParams output_ccrepe_data
 #'
 #' @export
-write.interaction_table <- function(significant_interactions, filename,
-                                     csv_option = "2") {
-  if (csv_option == "2") {
-    write.csv2(significant_interactions, file = filename, row.names = FALSE)
+write.interaction_table <-
+  function(significant_interactions,
+           filename,
+           csv_option = "2") {
+    if (csv_option == "2") {
+      write.csv2(significant_interactions,
+                 file = filename,
+                 row.names = FALSE)
+    }
+    else {
+      write.csv(significant_interactions,
+                file = filename,
+                row.names = FALSE)
+    }
   }
-  else {
-    write.csv(significant_interactions, file = filename, row.names = FALSE)
-  }
-}
 #' @name summary.interaction_table
 #'
 #' @title Create summary of \code{interaction_table}
@@ -305,12 +370,16 @@ write.interaction_table <- function(significant_interactions, filename,
 #'
 #' @export
 summary.interaction_table <- function(object, ...) {
-  proportion_negative <- ifelse(attr(object,'signed'),
-                                sum(object$sim.score < 0) / nrow(object), NA)
+  proportion_negative <- ifelse(attr(object, 'signed'),
+                                sum(object$sim.score < 0) / nrow(object),
+                                NA)
   number_significant <- nrow(object)
   c(
     attributes(object)[c("measure_name", "signed", "measure_type")],
-    list(number_significant = number_significant, proportion_negative = proportion_negative)
+    list(
+      number_significant = number_significant,
+      proportion_negative = proportion_negative
+    )
   )
 }
 
@@ -365,7 +434,13 @@ collapse_taxonomy <- function(phyloseq) {
   }
   taxonomy_frame <- data.frame(taxonomy_table)
   # Makes a contatented string of the taxonomies of each OTU
-  taxonomy <- apply(taxonomy_frame, MARGIN = 1, FUN = function(x) paste(x, collapse = ","))
+  taxonomy <-
+    apply(
+      taxonomy_frame,
+      MARGIN = 1,
+      FUN = function(x)
+        paste(x, collapse = ",")
+    )
   names(taxonomy) <- rownames(taxonomy_frame)
   return(taxonomy)
 }
@@ -389,30 +464,40 @@ collapse_taxonomy <- function(phyloseq) {
 #' data("soilrep")
 #' s <- 1:nsamples(soilrep)
 #' scale_by_column(soilrep, s)
-scale_by_column <- function(object, column){
-  if(is.character(column)){
-    if(length(column) != 1){
+scale_by_column <- function(object, column) {
+  if (is.character(column)) {
+    if (length(column) != 1) {
       stop("The input must be a character vector of length 1")
     }
-    if(column %in% phyloseq::sample_variables(object) %>% magrittr::not()){
+    if (column %in% phyloseq::sample_variables(object) %>% magrittr::not()) {
       stop("The selected column does not exist in the sample data")
     }
     scale_vector <- sample_data(object)[[column]]
   }
   else{
-    if(length(column) != nsamples(object)){
+    if (length(column) != nsamples(object)) {
       stop("The length of the input must equal the number of samples")
     }
     scale_vector <- column
   }
-  if(phyloseq::taxa_are_rows(object)){
-    OTU_table <- scale(phyloseq::otu_table(object),center = FALSE, scale = 1/scale_vector) %>%
+  if (phyloseq::taxa_are_rows(object)) {
+    OTU_table <-
+      scale(
+        phyloseq::otu_table(object),
+        center = FALSE,
+        scale = 1 / scale_vector
+      ) %>%
       phyloseq::otu_table(taxa_are_rows = TRUE)
   }
   else{
-    OTU_table <- t(scale(t(phyloseq::otu_table(object)),center = FALSE, scale = 1/scale_vector)) %>%
+    OTU_table <-
+      t(scale(
+        t(phyloseq::otu_table(object)),
+        center = FALSE,
+        scale = 1 / scale_vector
+      )) %>%
       phyloseq::otu_table(taxa_are_rows = FALSE)
   }
- otu_table(object) <- OTU_table
- return(object)
+  otu_table(object) <- OTU_table
+  return(object)
 }
